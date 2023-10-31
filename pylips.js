29b6c4f183566c324ac633e984a50e054c1d5b42
Julian Wiche <julian.wiche@kriko.com>
2023-10-31 12:14:23 +0100

    -Cloning pylips into current node directory -Injected current path into node environment -no need to manually download pylips anymore (when everything works :D)
    	modified:   package.json
    	modified:   pylips.js
    	modified:   subflow.json

--- pylips.js
+++ pylips.js
@@ -5,5 +5,11 @@
      const subflowFile = path.join(__dirname,"subflow.json");
      const subflowContents = fs.readFileSync(subflowFile);
      const subflowJSON = JSON.parse(subflowContents);
+     const pylipsDir = path.join(__dirname, "pylips", "pylips.py")
+     subflowJSON.env.push(    {
+        "name": "path",
+        "type": "str",
+        "value": pylipsDir
+    });
      RED.nodes.registerSubflow(subflowJSON);
  }
