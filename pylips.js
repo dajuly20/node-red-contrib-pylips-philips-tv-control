4fcde4f7062b91a4083494569f3a55c357b552ce
Julian Wiche <julian.wiche@kriko.com>
2023-10-31 14:12:40 +0100

    added Napa install

--- pylips.js
+++ pylips.js
@@ -5,7 +5,7 @@
      const subflowFile = path.join(__dirname,"subflow.json");
      const subflowContents = fs.readFileSync(subflowFile);
      const subflowJSON = JSON.parse(subflowContents);
-     const pylipsDir = path.join(__dirname, "pylips", "pylips.py")
+     const pylipsDir = path.join(__dirname, "node_modules", "pylips", "pylips.py")
      subflowJSON.env.push(    {
         "name": "path",
         "type": "str",
